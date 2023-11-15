4c4d9daa71e41197750c1860d16783c6b01dbaf1
Jarren Chaves Vizcaino <jarren.chaves.vizcaino@est.una.ac.cr>
2023-11-14 23:08:18 -0600

    Comentario

--- Proyecto-Aeropuerto/Principal.cpp
+++ Proyecto-Aeropuerto/Principal.cpp
@@ -1,32 +1,9 @@
-#include"InterfazEmpleado.h"
-#include"MenuPrincipal.h"
-#include"InterfazAvion.h"
-#include"InterfazContrato.h"
-#include"InterfazReporteConsulta.h"
-#include"InterfazEnlace.h"
-#include"InterfazArchivo.h"
+// Estudiantes:
+// Jarren Chaves Vizcaino
+// Lesber Huerta Cornejo
 #include"ControladoraPrincipal.h"
-
 int main() {
-
-	
-	//Aeropuerto* aeropuerto = new Aeropuerto("Aeropuerto Tilin", "11114-2314", "9861-4673", *contAviones, *contContratos, *contEmpleados,*contPlazas); // eso tilin 
-	//ofstream empleado, avion, contrato, plaza;
-	//ifstream empleadoEsc, avionEsc, contratoEsc, plazaEsc;
-
-	int d,m,a;
-	
-	
-	// Antes de entrar en el sistema necesitamos la fecha actual
-	// para poder saber si un contrato de plazo fijo tiene mas de 2 anios
-	// a asi saber si es elegible o no.
-
-	cout << "Digite la fecha de acutal: " << endl;
-	cout << "Dia: "; cin >> d;
-	cout << "Mes: "; cin >> m;
-	cout << "Anio: "; cin >> a;
-	ControladoraPrincipal* Controladora = new ControladoraPrincipal(d, m, a);
+	ControladoraPrincipal* Controladora = new ControladoraPrincipal(14, 11, 2023);
 	Controladora->control0();
-
 	return 0;
 }
